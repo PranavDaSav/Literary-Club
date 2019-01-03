ef8759cbfae78253b792a0c690cae2a960a62957
PranavDaSav <41297023+PranavDaSav@users.noreply.github.com>
2019-01-02 19:51:26 -0600

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,20 @@
+document.getElementById('BUTTON_soloPlay').onclick = function() {
+  window.open(
+   'file:///Users/pranav/Desktop/Lit%20Club/SoloPlay.html')
+  '_blank'
+
+};
+
+document.getElementById('BUTTON_duoPlay').onclick = function()  {
+  window.open(
+    'file:///Users/pranav/Desktop/Lit%20Club/DuoPlay.html')
+  '_blank'
+
+};
+
+document.getElementById('BUTTON_groupPlay').onclick = function()  {
+  window.open(
+    'file:///Users/pranav/Desktop/Lit%20Club/GroupPlay.html')
+  '_blank'
+
+};
